c8c13c928f09848c3351fbb8f6e5a77083723c7f
Lucila Zabala <lucilazabala.dev@gmail.com>
2025-12-14 20:13:00 -0300

    resolucion de ejercicios del 1 al 5

---
+++ tp.sql
@@ -0,0 +1,43 @@
+-- Ejercicio 1: Crear Base de Datos
+CREATE DATABASE veterinaria_patitas_felices;
+
+USE veterinaria_patitas_felices;
+
+-- Ejercicio 2: Crear tabla duenos
+CREATE TABLE duenos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    direccion VARCHAR(100)
+);
+
+-- Ejercicio 3 – Crear tabla mascotas
+CREATE TABLE mascotas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    especie VARCHAR(30) NOT NULL,
+    fecha_nacimiento DATE,
+    id_dueno INT,
+    FOREIGN KEY (id_dueno) REFERENCES duenos(id)
+);
+
+-- Ejercicio 4 – Crear tabla veterinarios
+CREATE TABLE veterinarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    matricula VARCHAR(20) NOT NULL UNIQUE,
+    especialidad VARCHAR(50) NOT NULL
+);
+
+-- Ejercicio 5 – Crear tabla historial_clinico
+CREATE TABLE historial_clinico(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_mascota INT,
+    FOREIGN KEY (id_mascota) REFERENCES mascotas(id),
+    id_veterinario INT,
+    FOREIGN KEY (id_veterinario) REFERENCES veterinarios(id),
+    fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    descripcion VARCHAR(250) NOT NULL
+);
